b49a92aa0a7760af6d566310c4072b844f8e5fd0
YuuBey <abayugnz@gmail.com>
2026-06-21 09:04:57 +0700

    fixed eror

--- api/index.js
+++ api/index.js
@@ -2396,7 +2396,7 @@ app.get("/api/cavporn/player", (req, res) => {
 const distPath = path.join(__dirname, "../dist");
 app.use(express.static(distPath));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
